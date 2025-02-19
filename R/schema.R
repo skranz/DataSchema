b258bb814123899e7e0ca6f8294d674c2a672921
skranz-auto <xsx50@uni-ulm.de>
2025-02-19 06:44:45 +0000

    Update from server

--- R/schema.R
+++ R/schema.R
@@ -1,10 +1,10 @@
 
 example = function() {
-  obj = sx_obj(name="", num=5)
   obj = schema_obj(list(name="", num=5))
   arr = schema_arr(schema_obj(list(name="", num=5)))
   arr = schema_arr(list(name="", num=5.2))
 
+  schema_str("my_descr", parse=TRUE)
 
   schema = schema_arr(
     list(
@@ -12,26 +12,21 @@ example = function() {
       country = "France",
       famous_building="Eiffel Tower",
       population = schema_num("Population of the country in million inhabitants"),
-      facts = sx_arr(name="fact1", description="fact_description")
+      facts = schema_arr(list(name="fact1", description="fact_description"))
     )
   )
-
-  schema = new_schema(
-    sx_arr(facts = arr_resp(name="fact1", description="fact_description"))
-  )
-
 }
 
-sx_arr = function(...) {
-  x = schema_arr(items = sx_obj(...))
-  x
-}
+# sx_arr = function(...) {
+#   x = schema_arr(items = sx_obj(...))
+#   x
+# }
 
 
-sx_obj = function(...) {
-  x = schema_obj(list(...))
-  x
-}
+# sx_obj = function(...) {
+#   x = schema_obj(list(...))
+#   x
+# }
 
 
 
@@ -82,29 +77,32 @@ to_schema = function(x) {
 #   x
 # }
 
-schema_str = function(description=NULL, is_key=NULL) {
-  x = nn_locals_to_list()
+schema_str = function(description=NULL, allow_null=FALSE, enum=NULL, pattern=NULL, minLength=NULL, maxLength=NULL,is_key=NULL, ...) {
+  x = nn_locals_to_list(...)
   x$type = "string"
+  if (allow_null) x$type = c(x$type, "null")
   class(x) = c("schema_str",  "schema", "list")
   x
 }
 
-schema_int = function(description=NULL,is_key=NULL, minimum=NULL, exclusiveMinimum=NULL, maximum=NULL, exclusiveMaximum=NULL) {
-  x = nn_locals_to_list()
+schema_int = function(description=NULL, allow_null = FALSE, enum=NULL, minimum=NULL, exclusiveMinimum=NULL, maximum=NULL, exclusiveMaximum=NULL,is_key=NULL,  ...) {
+  x = nn_locals_to_list(...)
   x$type = "integer"
+  if (allow_null) x$type = c(x$type, "null")
   class(x) = c("schema_int","schema", "list")
   x
 }
 
-schema_num = function(description=NULL,is_key=NULL, minimum=NULL, exclusiveMinimum=NULL, maximum=NULL, exclusiveMaximum=NULL) {
-  x = nn_locals_to_list()
+schema_num = function(description=NULL, allow_null=FALSE, enum=NULL, minimum=NULL, exclusiveMinimum=NULL, maximum=NULL, exclusiveMaximum=NULL,is_key=NULL,  ...) {
+  x = nn_locals_to_list(...)
   x$type = "numeric"
+  if (allow_null) x$type = c(x$type, "null")
   class(x) = c("schema_num", "schema", "list")
   x
 }
 
-schema_arr = function(items, description=NULL, minItems=NULL,uniqueItems=NULL) {
-  x = nn_locals_to_list()
+schema_arr = function(items, description=NULL, minItems=NULL,maxItems=NULL, uniqueItems=NULL, ...) {
+  x = nn_locals_to_list(...)
   restore.point("schema_arr")
   if (!is_schema(items) & is.list(items)) {
     x$items = schema_obj(items)
@@ -116,8 +114,8 @@ schema_arr = function(items, description=NULL, minItems=NULL,uniqueItems=NULL) {
   x
 }
 
-schema_obj = function(properties, description=NULL) {
-  x = nn_locals_to_list()
+schema_obj = function(properties, description=NULL, ...) {
+  x = nn_locals_to_list(...)
   restore.point("schema_obj")
   if (length(properties)==0) stop("An object schema needs at least one property.")
   x$properties = lapply(properties, to_schema)
@@ -126,6 +124,21 @@ schema_obj = function(properties, description=NULL) {
   x
 }
 
+schema_obj_slice = function(x, vars) {
+  prob = x$properties
+  vars = intersect(names(prob), vars)
+  x$properties = x$properties[vars]
+  x
+}
+
+schema_obj_merge = function(x,y) {
+  x = to_schema(x)
+  y = to_schema(y)
+
+  yvar = setdiff(names(y$properties), names(x$properties))
+  x$properties = c(x$properties, y$properties[y])
+  x
+}
 #' Create an Object Response Template
 #'
 #' This function creates a template for a single JSON object response.
--- R/tools.R
+++ R/tools.R
@@ -1,34 +1,22 @@
-nn_locals_to_list <- function(ignore_null=TRUE) {
+nn_locals_to_list <- function(...) {
   # Get the environment of the calling function
   caller_env <- parent.frame()
 
-  # List all objects (including hidden ones) in the caller's environment
-  object_names <- ls(envir = caller_env, all.names = TRUE)
+  restore.point("shkhsdfhk")
+  # Retrieve all objects (including hidden ones) from the caller's environment
+  local_objs <- mget(ls(envir = caller_env, all.names = FALSE),
+                     envir = caller_env, inherits = FALSE)
 
-  # Retrieve the objects as a named list without inheriting from parent environments
-  object_list <- mget(object_names, envir = caller_env, inherits = FALSE)
-  if (ignore_null) {
-    is_null = sapply(object_list, is.null)
-    object_list = object_list[!is_null]
-  }
+  local_objs = local_objs[setdiff(names(local_objs), "...")]
+
+  # Capture any objects passed via ... to locals_to_list
+  extra <- list(...)
+  object_list = c(local_objs, extra)
+
+
+  is_null = sapply(object_list, is.null)
+  object_list = object_list[!is_null]
 
   return(object_list)
 }
 
-
-locals_to_list <- function(ignore_null=FALSE) {
-  # Get the environment of the calling function
-  caller_env <- parent.frame()
-
-  # List all objects (including hidden ones) in the caller's environment
-  object_names <- ls(envir = caller_env, all.names = TRUE)
-
-  # Retrieve the objects as a named list without inheriting from parent environments
-  object_list <- mget(object_names, envir = caller_env, inherits = FALSE)
-  if (ignore_null) {
-    is_null = sapply(object_list, is.null)
-    object_list = object_list[!is_null]
-  }
-
-  return(object_list)
-}
